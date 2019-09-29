018d36fdf76eb814b5a5a3dc1490b1049d3846e2
renr <2468658910@qq.com>
2019-09-29 21:56:03 +0800

    Initial commit

---
+++ src/main/java/com/edu/yiguo/YiguoApplication.java
@@ -0,0 +1,13 @@
+package com.edu.yiguo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiguoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YiguoApplication.class, args);
+    }
+
+}
